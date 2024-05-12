f335c0b8a893fcebf4cf3f8a10aaa6ba69966be6
LouisBomal <louisbomal@outlook.com>
2024-05-12 19:39:34 +0200

    qq trucs en plus

--- app/route/autres.py
+++ app/route/autres.py
@@ -8,7 +8,7 @@ from fastapi.templating import Jinja2Templates
 from app.schemas.users import UserSchema
 from pydantic import ValidationError
 from app.login_manager import login_manager
-
+from app.services.achat import get_id_car
 
 
 router = APIRouter(prefix="/autres", tags=["Autres"])
@@ -19,3 +19,12 @@ templates = Jinja2Templates(directory="./templates")
 def home(request : Request, user:UserSchema = Depends(login_manager.optional)):  
     return templates.TemplateResponse('home.html', context={'request':request, 'current_user':user})
 
+@router.get('/paiement/{id}')
+def paiement(request : Request, id:str,user:UserSchema = Depends(login_manager.optional)):
+    car = get_id_car(id)
+    return templates.TemplateResponse('paiement.html' ,context={"request":request, "user":user, 'car':car})
+
+@router.post('/paiement/{id}')
+def paiement(request : Request, id:str, adresse:Annotated[str,Form()], titulaire:Annotated[str, Form()], numero:Annotated[str, Form()], pays:Annotated[str, Form()], date:Annotated[str, Form()], user:UserSchema = Depends(login_manager.optional)):
+    #rien n'est pour l'instant fait avec les coordonnées bancaires et le paiement
+    return RedirectResponse('/achat/catalogue', status_code=302)
